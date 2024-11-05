0c98c49cf168aa1968433e23ad01b19c38731e3d
Gregg Trunnell <gggtrunnell@gmail.com>
2024-11-05 16:59:39 -0600

    got the weapons loaded. work in the console

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // let fungusHP = 100;
 
 //need a starting health value for fungus
-//let fungusHp=document.querySelector("#hp-text");
+let fungusHp=100;
 let myAP=100;
 
 //writing some dummy variables to test things will probably turn into object var's
@@ -13,12 +13,47 @@ let entangleHp=9;//ap=23
 let dragonBladeHp=47;//ap=38
 let starFireHp=25;//ap=33
 
-function attacking(event){
-document.querySelector("#hp-meter").value-=dragonBladeHp;
-//line below will affect hp text but fills with nan
-document.querySelector("#hpText").innerText-=dragonBladeHp;
-console.log('attacking');
+
+
+// function attack(event){
+//     let fungusTotal = fungusHp;
+//     let apTotal = myAP;
+//     let scepterCost= apTotal-12;
+//     fungusHp=fungusTotal-arcaneSepterHp;
+//     myAP = apTotal - scepterCost;
+//     console.log('my fungus total', )
+// }
+
+//atticking function will take a one of the buttons and have it reduce our aP and hP
+function attacking(weapon){
+    console.log("weapon is", weapon);
+if (weapon === 'arcane-scepter'){
+    fungusHp-=14;
+    myAP-=12;
+    console.log('this is the new fungusHp',fungusHp,'new ap is:',myAP)
 }
+else if (weapon === 'entangle'){
+    fungusHp-=9;
+    myAP-=23;
+    console.log('this is the new fungusHp',fungusHp,'new ap is:',myAP)
+}
+else if (weapon === 'dragon-blade'){
+    fungusHp-=47;
+    myAP-=38;
+    console.log('this is the new fungusHp',fungusHp,'new ap is:',myAP)
+}
+else if ('star-fire'){
+    fungusHp-=25;
+    myAP-=33;
+    console.log('this is the new fungusHp',fungusHp,'new ap is:',myAP)}
+
+//need code to reduce fungus hP
+
+}//*end of attacking function
+      
+
+
+ 
 //need to subtractHp from fungus based on weaponsHP 
 //will need to subtract weapon cost from myAP
 //could maybe use an array for myAp
